e4ecd32657cb77616ce456b0a68b7e32aee66ca2
55131032+TreasonMay@users.noreply.github.com <55131032+TreasonMay@users.noreply.github.com>
2020-12-01 18:21:03 +0000

    Added the ability to mark messages as read. Reworked message archiving to use graphql

--- FireflyAPI/messages.py
+++ FireflyAPI/messages.py
@@ -8,7 +8,7 @@ class Message(DiscretelyAuthenticatedObject):
         DiscretelyAuthenticatedObject.__init__(self, auth_blob)
         self.id = message_data["id"]
         self.message_text = message_data["body"]
-        self.read = message_data["read"]
+        self.is_read = message_data["read"]
         self.sender = message_data["from"]["name"]
         self.archived = message_data["archived"]
         self.date = utils.firefly_timestamp_to_date_time(message_data["sent"])
@@ -16,11 +16,18 @@ class Message(DiscretelyAuthenticatedObject):
     def __set_archive_status(self, archive):
         params = {"ffauth_device_id": self._DiscretelyAuthenticatedObject__device_id,
                   "ffauth_secret": self._DiscretelyAuthenticatedObject__device_token}
-        data = {f"{'un' * (not archive)}archive": "yes"}
-        requests.post(
-            self._DiscretelyAuthenticatedObject__portal + "/messages/" + str(self.id),
-            params=params, data=data)
+        data = {"data": str(
+            "mutation M { result: messages(ids: [" + str(self.id) + "], user_guid: \"" +
+            self._DiscretelyAuthenticatedObject__guid + "\", new_archive: " + str(archive * 1) + ")")}
+        requests.post(self._DiscretelyAuthenticatedObject__portal + "/_api/1.0/graphql", params=params, data=data)
 
+    def __set_read_status(self, read):
+        params = {"ffauth_device_id": self._DiscretelyAuthenticatedObject__device_id,
+                  "ffauth_secret": self._DiscretelyAuthenticatedObject__device_token}
+        data = {"data": str(
+            "mutation M { result: messages(ids: ["+str(self.id)+"], user_guid: \"" +
+            self._DiscretelyAuthenticatedObject__guid+"\", new_read: " + str(read * 1) + ")")}
+        requests.post(self._DiscretelyAuthenticatedObject__portal + "/_api/1.0/graphql", params=params, data=data)
     def archive(self):
         """
         Archives the message.
@@ -32,3 +39,15 @@ class Message(DiscretelyAuthenticatedObject):
         Un-archives the message.
         """
         self.__set_archive_status(False)
+
+    def read(self):
+        """
+        Marks the message as read.
+        """
+        self.__set_read_status(True)
+
+    def unread(self):
+        """
+        Marks the message as un-read.
+        """
+        self.__set_read_status(False)
